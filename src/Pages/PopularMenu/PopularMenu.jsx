77b2eb834e44122c8e23bbd8021b04b4113af8a1
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2024-01-05 22:04:56 +0600

    add popular menu

---
+++ src/Pages/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
+
+const PopularMenu = () => {
+  return (
+    <div>
+      <SectionTitle heading='From Our Menu' subHeading='Popular Items'></SectionTitle>
+    </div>
+  );
+};
+
+export default PopularMenu;

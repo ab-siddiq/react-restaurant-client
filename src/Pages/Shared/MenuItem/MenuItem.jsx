1fd0d387c83db8a44f602dbb5e10377b5f992c28
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2024-01-10 01:40:56 +0600

    fix responsive issue of homepage

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const MenuItem = ({item}) => {
   const {name,image,price,recipe} = item
   return (
-    <div className='flex space-x-4 flex-col sm:flex-row md:flex-row justify-center items-center'>
+    <div className='flex space-x-4 flex-col sm:flex-row md:flex-row justify-center items-center sm:items-start sm:justify-end'>
       <img className='w-[100px]' style={{borderRadius: '0 200px 200px 200px'}} src={image} alt="" />
-      <div className="flex flex-col justify-center items-center mt-6">
+      <div className="flex flex-col justify-center items-center sm:items-start  mt-6 sm:mt-0">
       <h3 className="font-bold text-xl text-yellow-500">{name}................</h3>
       <p className="text-yellow-600">{recipe}</p>
       </div>

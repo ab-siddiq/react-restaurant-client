5389b4e606995c52b1bb6493c3119df3c062f7e9
Md. Abu Bakkar Siddiq <siddiq.aiub@gmail.com>
2024-01-10 22:27:23 +0600

    create context for authentication

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
-import {  HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-    <div className="max-w-screen-xl mx-auto">
-      <RouterProvider router={router}></RouterProvider>
-    </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-xl mx-auto">
+          <RouterProvider router={router}></RouterProvider>
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import { app } from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app); 
+
+const AuthProvider = ({ children }) => {
+  
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const authInfo = {
+    user,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
